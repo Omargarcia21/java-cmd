9208c6023b8e0fe82425220a5a6d04d5639f0378
Omar <omar.garcia.21@hotmail.com>
2024-07-31 18:25:10 -0600

    Añadiendo nuevo metodo a clase Principal.java

--- src/main/java/com/ejemplo/Principal.java
+++ src/main/java/com/ejemplo/Principal.java
@@ -3,6 +3,10 @@ package com.ejemplo;
 public class Principal {
 	public static void main (String[] args) {
 		System.out.println("¡Hola, mundo desde ps!");
+		imprimirMsj();
 
+	}
+	public static void imprimirMsj() {
+		System.out.println("Mensaje desde la rama feature");
 	}
 }
